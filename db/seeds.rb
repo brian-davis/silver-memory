d33102281dc180f6e927113beab9dfa6c1a6e6a1
Brian Davis <brian.davis@icentris.com>
2022-04-03 11:19:18 -0700

    use importer in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Rails.logger.tagged("RAKE").tagged("SEED").info { "START IMPORT FROM seeds.json" }
+json_string = File.read(Rails.root.join("db", "seeds.json"))
+data = JSON.parse(json_string)
+RestaurantImporter.import!(data)
